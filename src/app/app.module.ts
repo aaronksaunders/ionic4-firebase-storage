16ef6591bc4bc77a8884b951124fa2f0a0f2f088
aaron saunders <aaronksaunders@gmail.com>
2018-09-11 00:53:44 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,16 @@ import { StatusBar } from "@ionic-native/status-bar/ngx";
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
 
+import { Camera } from '@ionic-native/camera/ngx';
+import { File } from '@ionic-native/file/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
+    File,
+    Camera,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,106 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Camera, CameraOptions } from "@ionic-native/camera/ngx";
+import { File } from "@ionic-native/file/ngx";
+
+// FIREBASE
+import * as firebase from "firebase";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  result;
 
+  constructor(private camera: Camera, private file: File) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    firebase.initializeApp({
+
+    });
+  }
+
+  async pickImage() {
+    const options: CameraOptions = {
+      quality: 80,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.result = await this.camera.getPicture(options);
+    let blobInfo = await this.makeFileIntoBlob(this.result);
+    this.uploadToFirebase(blobInfo)
+      .then(_result => {
+        this.result = _result;
+        alert("File Upload Success")
+      })
+      .catch(e => (this.result = e));
+  }
+
+  // FILE STUFF
+  makeFileIntoBlob(_imagePath) {
+    // INSTALL PLUGIN - cordova plugin add cordova-plugin-file
+    return new Promise((resolve, reject) => {
+      let fileName = "";
+      this.file
+        .resolveLocalFilesystemUrl(_imagePath)
+        .then(fileEntry => {
+          let { name, nativeURL } = fileEntry;
+
+          // get the path..
+          let path = nativeURL.substring(0, nativeURL.lastIndexOf("/"));
+          console.log("path", path);
+          console.log("fileName", name);
+
+          fileName = name;
+
+          // we are provided the name, so now read the file into
+          // a buffer
+          return this.file.readAsArrayBuffer(path, name);
+        })
+        .then(buffer => {
+          // get the buffer and make a blob to be saved
+          let imgBlob = new Blob([buffer], {
+            type: "image/jpeg"
+          });
+          console.log(imgBlob.type, imgBlob.size);
+          resolve({
+            fileName,
+            imgBlob
+          });
+        })
+        .catch(e => reject(e));
+    });
+  }
+
+  /**
+   *
+   * @param _imageBlobInfo
+   */
+  uploadToFirebase(_imageBlobInfo) {
+    return new Promise((resolve, reject) => {
+      let fileRef = firebase.storage().ref("images/" + _imageBlobInfo.fileName);
+
+      let uploadTask = fileRef.put(_imageBlobInfo.imgBlob);
+
+      uploadTask.on(
+        "state_changed",
+        _snapshot => {
+          console.log("snapshot progess " + JSON.stringify(_snapshot));
+        },
+        _error => {
+          console.log(_error);
+          reject(_error);
+        },
+        () => {
+          // completion...
+          resolve(uploadTask.snapshot);
+        }
+      );
+    });
+  }
 }
